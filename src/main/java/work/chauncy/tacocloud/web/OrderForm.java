88e1fe2f80b1556f1caeab3cdf36a5cfeb65a23c
Chauncy <baixinke@qq.com>
2023-08-21 21:09:30 +0800

    Processing form submission

---
+++ src/main/java/work/chauncy/tacocloud/web/OrderForm.java
@@ -0,0 +1,31 @@
+package work.chauncy.tacocloud.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+
+import lombok.extern.slf4j.Slf4j;
+import work.chauncy.tacocloud.TacoOrder;
+
+@Slf4j
+@Controller
+@RequestMapping("/orders")
+@SessionAttributes("tacoOrder")
+public class OrderForm {
+    @GetMapping("/current")
+    public String orderForm() {
+        return "orderForm";
+    }
+
+    @PostMapping
+    public String processOrder(TacoOrder order,
+            SessionStatus sessionStatus) {
+        log.info("Order submitted: {}", order);
+        sessionStatus.setComplete();
+
+        return "redirect:/";
+    }
+}
